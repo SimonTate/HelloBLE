e1f9b8bfed5ef726f4fda04f1165a948114235a5
Simon Tate <simontate94@gmail.com>
2024-01-22 20:19:04 +0000

    Add sample code
    Initialise bluetooth adapter with SimpleBLE sample code.

--- src/hello-ble.cpp
+++ src/hello-ble.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <simpleble/SimpleBLE.h>
 
-int main() {
-	std::cout << "hello, world" << std::endl;
+int main(int argc, char** argv) {
+	if (!SimpleBLE::Adapter::bluetooth_enabled()) {
+		std::cout << "Bluetooth is not enabled" << std::endl;
+		return 1;
+	}
+
+	auto adapters = SimpleBLE::Adapter::get_adapters();
+	if (adapters.empty()) {
+		std::cout << "No Bluetooth adapters found" << std::endl;
+		return 1;
+	}
+
+	// Use the first adapter
+	auto adapter = adapters[0];
+
+	// Do something with the adapter
+	std::cout << "Adapter identifier: " << adapter.identifier() << std::endl;
+	std::cout << "Adapter address: " << adapter.address() << std::endl;
 
 	return 0;
 }
+
